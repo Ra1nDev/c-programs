7b33ecfc0145e4c3d5dd728d29b60dea7b3dadb4
Christopher <32487758+racoon31415@users.noreply.github.com>
2017-10-03 10:21:54 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 int main (void) {
 
   //Variables
-  int m;
-  int z;
+  int m=0;
+  int z=0;
   int r;
   
-  printf("\n|||--- Welcome to the Multiplication app. ---|||\n");
-  printf("Please enter two numbers both followed by the return key:");
-  scanf("%d", m);
-  scanf("%d", z);
-  printf("\n Here is your result: %d\n\n Thank you for using the multiplication app",m*z=r);
-
-  return 0;
-
+  printf("\n|||--- Welcome to the Multiplication app ---|||\n\n");
+  printf("Please enter your first number followed by the return key: ");
+  scanf("%i",&m);
+  printf("\n\nPlease enter your second number followed by the return key: ");
+  scanf("%i",&z);
+  r = m * z;
+  printf("\n Here is your result: %d\n\n Thank you for using the multiplication app\n\n", r);
+	return 0;
 }
