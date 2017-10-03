54b2987b2e3d4a423414839c3d883886eb245b11
Christopher <32487758+racoon31415@users.noreply.github.com>
2017-10-03 09:36:14 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main (void) {
+
+  //Variables
+  int m;
+  int z;
+  int r;
+  
+  printf("\n|||--- Welcome to the Multiplication app. ---|||\n");
+  printf("Please enter two numbers both followed by the return key:");
+  scanf("%d", m);
+  scanf("%d", z);
+  printf("\n Here is your result: %d\n\n Thank you for using the multiplication app",m*z=r);
+
+  return 0;
+
+}
